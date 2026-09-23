eba21fb088446e6cdc52cd35ac4219502bf426f2
andrew-nerney <andrew.nerney2@gmail.com>
2026-09-23 11:27:56 -0400

    defining resources

--- mcp_server.py
+++ mcp_server.py
@@ -43,7 +43,25 @@ def edit_document(
 
 
 # TODO: Write a resource to return all doc id's
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
 # TODO: Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def get_doc_contents(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+)
+
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
